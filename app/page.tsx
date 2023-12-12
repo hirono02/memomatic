6895feb1649d215e92404c23a6c4a5d804caccf9
hirono02 <147478319+hirono02@users.noreply.github.com>
2023-12-12 18:41:26 +0800

    Basic behaviour of textarea

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import "../app/css/home.css";
+
 export default function Home() {
   return (
-    <h1>This is the Home Page</h1>
-  )
+    <>
+      <div className="grid place-content-center pt-3">
+        <textarea
+          className="border p-2 rounded-xl textareas"
+          name="message"
+          placeholder="Enter content"
+          rows={10}
+          cols={90}
+        />
+      </div>
+    </>
+  );
 }
