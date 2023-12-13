8e5c20d7d1cdc4c9dda07accda5715d5c655f2aa
hirono02 <147478319+hirono02@users.noreply.github.com>
2023-12-13 22:54:21 +0800

    feat: added some form of ui validation for word count

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -1,16 +1,12 @@
-'use client'
+"use client";
 import { useMessage } from "@/contexts/messageContext";
 import OpenAI from "openai";
-
-
 
 interface TextAreaProps {
   apiKey: string | undefined;
 }
 
-
-
-const TextArea: React.FC<TextAreaProps> = ( { apiKey } ) => {
+const TextArea: React.FC<TextAreaProps> = ({ apiKey }) => {
   const { message, setMessage, wordCount, setWordCount } = useMessage();
 
   const updateMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -19,23 +15,23 @@ const TextArea: React.FC<TextAreaProps> = ( { apiKey } ) => {
   };
 
   const openai = new OpenAI({ apiKey: apiKey, dangerouslyAllowBrowser: true });
-   const generateFlashcards = async () => {
-     const completion = await openai.chat.completions.create({
-       messages: [
-         {
-           role: "system",
-           content:
-             "You are a flashcard generator. The user will input text and you are to use that text and context of that text and generate 5 flashcards regarding it in a question, answer format. Give the answers in json format as well.",
-         },
-         {
-           role: "user",
-           content: message,
-         },
-       ],
-       model: "gpt-3.5-turbo",
-     });
-     console.log(completion.choices[0]);
-   };
+  const generateFlashcards = async () => {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a flashcard generator. The user will input text and you are to use that text and context of that text and generate 5 flashcards regarding it in a question, answer format. Give the answers in json format as well.",
+        },
+        {
+          role: "user",
+          content: message,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(completion.choices[0]);
+  };
 
   return (
     <>
@@ -49,13 +45,22 @@ const TextArea: React.FC<TextAreaProps> = ( { apiKey } ) => {
         onChange={updateMessage}
       />
       <div className="flex justify-between pt-2">
-        <button onClick={generateFlashcards} className=" bg-slate-400 hover:bg-slate-500 rounded-md p-1">
+        <button
+          onClick={generateFlashcards}
+          className=" bg-slate-400 hover:bg-slate-500 rounded-md p-1"
+        >
           <p className="m-1 text-sm">Generate</p>
         </button>
-        <p className="text-xs text-gray-400">{wordCount}/1000 characters</p>
+        <p
+          className={
+            wordCount > 2000 ? "text-xs text-red-400" : "text-xs text-gray-400"
+          }
+        >
+          {wordCount}/2000 characters
+        </p>
       </div>
     </>
   );
-}
+};
 
-export default TextArea
+export default TextArea;
